4e014ff7b96fa85887d85bce5d45f036784371ec
sanjanachaudhary88 <68250181+sanjanachaudhary88@users.noreply.github.com>
2021-11-26 15:07:21 +0530

    Add files via upload

---
+++ home.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class home : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        if (txtlogin.Text == "")
+        {
+            lblerr.Text = "Please enter username";
+
+        }
+        if(txtpass.Text == "")
+        {
+            lblerrpass.Text = "Please enter Password";
+        }
+        else
+        {
+            string login=txtlogin.Text;
+            string pass=txtpass.Text;
+            if(login=="purvesh" && pass == "pass@123")
+            {
+                Response.Redirect("Firstform.aspx");
+            }
+            else
+            {
+                lblerr2.Text = "invalid username or password is entered";
+            }
+        }
+    }
+}
